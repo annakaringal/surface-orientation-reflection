124c53b943a3132322339d1b132377b2de1c499f
Anna Karingal <anna.karingal@gmail.com>
2015-11-08 11:30:20 -0500

    create Sphere class

---
+++ Sphere.h
@@ -0,0 +1,22 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+#include "pgm/Image.h"
+#include "Validation.h"
+
+using namespace std;
+
+class Sphere{
+
+public:
+
+private:
+  pair <float, float> center;
+  int radius; 
+
+
+#endif
