9328e85e25a3def9991d7e3acd9a6e6be419e192
Anna Karingal <anna.karingal@gmail.com>
2015-11-08 10:57:27 -0500

    read inputs from command line

---
+++ s1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "BinaryImage.h"
+#include "pgm/Image.h"
+#include "Validation.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    if (argc != 3) {
+      cerr << "ERROR: Invalid number of arguments." << endl;
+      exit(-1);
+    }
+
+    const char* input_img_fname = argv[1];
+    const char* output_img_fname = argv[3];
+
+    // Check if threshold is valid int and set to variable
+    if (!isValidType<int, const char*>(argv[2])){
+      cerr << "ERROR: Invalid threshold number." << endl;
+      exit(-1);
+    }
+    int threshold = atoi(argv[2]);
+
+    // Read image from input file 
+    Image* input_img = new Image;
+    if (readImage(input_img, input_img_fname) < 0) {
+      cerr << "ERROR: Something went wrong reading the input image" << endl;
+      exit(-1);
+    }
+
+
+    if (writeImage(input_img, output_img_fname) < 0){
+      cerr << "ERROR: Something went wrong writing the output image" << endl;
+      exit(-1);
+    }
+
+    delete input_img;
+
+    return 0;
+}
