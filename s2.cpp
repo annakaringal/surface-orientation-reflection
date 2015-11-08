dda9619173be1251da0118398c444f7310c64e14
Anna Karingal <anna.karingal@gmail.com>
2015-11-08 15:45:47 -0500

    remove const

--- s2.cpp
+++ s2.cpp
@@ -10,7 +10,6 @@
 using namespace std;
 
 int SPHERE_ARGS[] = {2,3,4};
-int NUM_SPHERES = 3;
 
 int main(int argc, const char * argv[]) {
     if (argc != 6) {
@@ -20,13 +19,13 @@ int main(int argc, const char * argv[]) {
     const char* input_params_fname = argv[1];
     const char* output_fname = argv[5];
     vector<const char*> sphere_fnames; 
-    for (int i=0; i < NUM_SPHERES; i++){
+    for (int i=0; i < argc - 3; i++){
         sphere_fnames.push_back(argv[SPHERE_ARGS[i]]);
     }
 
     // Create new sphere images from files, add to vector
     vector<Image*> sphere_images;
-    for(int i=0; i < NUM_SPHERES; i++){
+    for(int i=0; i < sphere_fnames.size(); i++){
         Image* img = new Image;
         if (readImage(img, sphere_fnames[i]) < 0) {
           cerr << "ERROR: Something went wrong reading the input image" << endl;
@@ -41,7 +40,7 @@ int main(int argc, const char * argv[]) {
     spheres.push_back(sphere_from_file);
 
     // Create remaining spheres, add to vector
-    for(int i=1; i < NUM_SPHERES; i++){
+    for(int i=1; i < sphere_images.size(); i++){
         Sphere* s = new Sphere(sphere_images[i], sphere_from_file->getCenter(), sphere_from_file->getRadius());
         spheres.push_back(s);
     }
@@ -56,7 +55,7 @@ int main(int argc, const char * argv[]) {
 
     // Write brightness vector of each sphere to file
     if (writef.is_open()) {
-        for(int i=0; i < NUM_SPHERES; i++){
+        for(int i=0; i < spheres.size(); i++){
             vector<int> ls = spheres[i]->findLightSource();
             writef << ls[0] << " ";
             writef << ls[1] << " ";
@@ -66,7 +65,7 @@ int main(int argc, const char * argv[]) {
     writef.close();
 
     // Clean up and return
-    for (int i=0; i < NUM_SPHERES; i++){
+    for (int i=0; i < spheres.size(); i++){
         delete spheres[i];
         delete sphere_images[i];
     }
